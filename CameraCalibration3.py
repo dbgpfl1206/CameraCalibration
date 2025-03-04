fde5b1a9f36efe8904227e277b47054b78c75d9e
dbgpfl1206 <dbgpfl1206@gmail.com>
2025-03-04 17:27:22 +0900

    commit

---
+++ CameraCalibration3.py
@@ -0,0 +1,51 @@
+# CameraCalibration_1 확인용 코드
+
+import numpy as np
+import cv2
+import glob
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((5*6,3), np.float32)
+objp[:,:2] = np.mgrid[0:6,0:5].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+images = glob.glob('*.jpg') # Insert Image Path
+
+for fname in images:
+    img = cv2.imread(fname)
+    #img = cv2.resize(img,(720,720))
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (6,5),None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (6,5), corners2,ret)
+        cv2.imshow('img',img)
+        cv2.waitKey(500)
+cv2.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+
+print("Camera matrix : \n") # 내부 카메라 행렬
+print(mtx)
+print("-------------------------------------------\n")
+
+print("dist : \n") # 렌즈 왜곡 계수(Lens distortion coefficients)
+print(dist)
+print("-------------------------------------------\n")
+
+tot_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+    tot_error += error
+print("total error: ", tot_error/len(objpoints))
